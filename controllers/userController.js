a77b60ff4690be2473496afb066cb17ed9753ff4
akku27-cse <janaa7223@gmail.com>
2024-05-13 20:36:42 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,7 +48,10 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .status(201)
     .cookie("token", "", {
       httpOnly: true,
+      
       expires: new Date(Date.now()),
+      secure:true,
+    sameSite:"None"
     })
     .json({
       success: true,
--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (user, statusCode, res, message) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true, // Set httpOnly to true
+    secure:true,
+    sameSite:"None"
   };
 
   res.status(statusCode).cookie("token", token, options).json({
